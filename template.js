b3aedacc6dfd9d3e3103d6b91d3bde7cfe870baf
Rémi de Juvigny <remi@hey.com>
2020-11-05 14:57:43 +0100

    Use template function instead of object

---
+++ template.js
@@ -0,0 +1,10 @@
+module.exports = function(scope) {
+  return {
+    package: {
+      dependencies: {
+        "mime-types": "^2.1.27",
+        "strapi-plugin-graphql": "3.2.5",
+      }
+    }
+  }
+}
